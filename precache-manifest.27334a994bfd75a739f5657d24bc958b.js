5756f30e1bbed667f4d326691bd173c67ebf5118
miguel-osuna <osuna.angulo.miguel@gmail.com>
2019-11-13 17:41:14 -0800

    Updates

---
+++ precache-manifest.27334a994bfd75a739f5657d24bc958b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88b41e7114289b47fec6d612dafb7734",
+    "url": "/Robofriends-PWA/index.html"
+  },
+  {
+    "revision": "46d5724c99e22bd29504",
+    "url": "/Robofriends-PWA/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "93a662ffe55439b1b1bb",
+    "url": "/Robofriends-PWA/static/css/main.f1734380.chunk.css"
+  },
+  {
+    "revision": "46d5724c99e22bd29504",
+    "url": "/Robofriends-PWA/static/js/2.3649d868.chunk.js"
+  },
+  {
+    "revision": "93a662ffe55439b1b1bb",
+    "url": "/Robofriends-PWA/static/js/main.5eb4d6ad.chunk.js"
+  },
+  {
+    "revision": "e7d2b7a8503bf04eae4f",
+    "url": "/Robofriends-PWA/static/js/runtime-main.51082eb8.js"
+  },
+  {
+    "revision": "d071190e622e1b361e8ca05d75da42e5",
+    "url": "/Robofriends-PWA/static/media/BelshawDonutRobot.d071190e.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/miguel-osuna/Robofriends-PWA/precache-manifest.9f2ce76e7761de7fee90f28b54bf128c.js"
+  "/Robofriends-PWA/precache-manifest.27334a994bfd75a739f5657d24bc958b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/miguel-osuna/Robofriends-PWA/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Robofriends-PWA/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
