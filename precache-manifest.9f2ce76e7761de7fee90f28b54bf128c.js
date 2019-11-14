0b3147b29eab671309db2de463b955cb8dc6adcf
miguel-osuna <osuna.angulo.miguel@gmail.com>
2019-11-13 17:30:17 -0800

    Updates

---
+++ precache-manifest.9f2ce76e7761de7fee90f28b54bf128c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ded76d68b2072a117a9fe06ce27d8713",
+    "url": "/miguel-osuna/Robofriends-PWA/index.html"
+  },
+  {
+    "revision": "46d5724c99e22bd29504",
+    "url": "/miguel-osuna/Robofriends-PWA/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8e7b10c551c466fdd3f7",
+    "url": "/miguel-osuna/Robofriends-PWA/static/css/main.375431b4.chunk.css"
+  },
+  {
+    "revision": "46d5724c99e22bd29504",
+    "url": "/miguel-osuna/Robofriends-PWA/static/js/2.3649d868.chunk.js"
+  },
+  {
+    "revision": "8e7b10c551c466fdd3f7",
+    "url": "/miguel-osuna/Robofriends-PWA/static/js/main.2d8eb7ae.chunk.js"
+  },
+  {
+    "revision": "3dc03fadb98c46096a05",
+    "url": "/miguel-osuna/Robofriends-PWA/static/js/runtime-main.5f6c5b04.js"
+  },
+  {
+    "revision": "d071190e622e1b361e8ca05d75da42e5",
+    "url": "/miguel-osuna/Robofriends-PWA/static/media/BelshawDonutRobot.d071190e.woff"
+  }
+]);
